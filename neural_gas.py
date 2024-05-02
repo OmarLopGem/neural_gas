a8308e0b70608d44d5892cfc15dbdb46159d68b0
Ricardo Gonzalez <ricardo_glez_14@icloud.com>
2024-05-01 19:52:51 -0600

    Several Fixes

--- neural_gas.py
+++ neural_gas.py
@@ -23,11 +23,11 @@ def neighborhood_function(idx, winner):
     return np.exp(-distance**2 / (2 * sigma**2))
 
 # Función principal para entrenar el modelo de Gas Neural
-def train_gas_neural(data, som_map, epochs, learning_rate, ax):
+def train_gas_neural(data, som_map, signals, learning_rate, ax):
     scat = ax.scatter(data[:, 0], data[:, 1], c='b', s=2)
     som_scatter = ax.scatter(som_map[:, 0], som_map[:, 1], c='r')
-    circle_outer = plt.Circle((0, 0), 0.7, color='gray', fill=False, linestyle='-', linewidth=1)
-    circle_inner = plt.Circle((0, 0), 0.5, color='gray', fill=False, linestyle='-', linewidth=1)
+    circle_outer = plt.Circle((0.5, 0.5), 0.35, color='gray', fill=False, linestyle='-', linewidth=1)
+    circle_inner = plt.Circle((0.5, 0.5), 0.25, color='gray', fill=False, linestyle='-', linewidth=1)
     ax.add_artist(circle_outer)
     ax.add_artist(circle_inner)
     
@@ -39,34 +39,38 @@ def train_gas_neural(data, som_map, epochs, learning_rate, ax):
         print(f'Gas Neural - Signal {frame + 1}')
         return som_scatter,
     
-    anim = FuncAnimation(ax.figure, update, frames=len(data), interval=0.1, blit=True, repeat=False)
+    anim = FuncAnimation(ax.figure, update, frames=signals, interval=0.1, blit=True, repeat=False)
+    ax.set_xlim(0, 1)  # Establecer límites de visualización en el eje x
+    ax.set_ylim(0, 1)  # Establecer límites de visualización en el eje y
     plt.show()
 
 # Función para generar datos aleatorios en un anillo
 def generate_data(num_samples):
-    radius = 0.5
+    radius_outer = 0.35
+    radius_inner = 0.25
     theta = np.random.uniform(0, 2*np.pi, num_samples)
-    x1 = (radius + 0.2 * np.random.rand(num_samples)) * np.cos(theta)
-    x2 = (radius + 0.2 * np.random.rand(num_samples)) * np.sin(theta)
+    r = np.sqrt(np.random.uniform(radius_inner**2, radius_outer**2, num_samples))
+    x1 = r * np.cos(theta) + 0.5
+    x2 = r * np.sin(theta) + 0.5
     data = np.column_stack((x1, x2))
     return data
 
 # Parámetros del modelo
 input_dim = 2
 output_dim = 350
-epochs = 40000
+signals = 40000
 learning_rate = 0.1
 
 # Inicializar el mapa de Gas Neural (unidades de salida)
 som_map = np.random.rand(output_dim, input_dim) * 2 - 1
 
 # Generar datos aleatorios en un anillo
-data = generate_data(40000)
+data = generate_data(signals)
 
 # Crear la figura y el eje
 fig, ax = plt.subplots()
 ax.set_xlabel('X1')
 ax.set_ylabel('X2')
 
-# Entrenar el modelo de Gas Neural con animación
-train_gas_neural(data, som_map, epochs, learning_rate, ax)
+# Entrenar el modelo de Gas Neural con animación y zoom
+train_gas_neural(data, som_map, signals, learning_rate, ax)
