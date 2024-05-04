4f9fc531f4703d79646a2e53be89ad095f1adebf
Ricardo Gonzalez <ricardo_glez_14@icloud.com>
2024-05-03 20:43:05 -0600

    cambios

--- elbueno.py
+++ elbueno.py
@@ -1,69 +1,73 @@
 import random
 import math
-import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-def plot_weights(pesos, e, iteracion):
+import matplotlib.pyplot as plt
+
+def plot_weights(weights, e, iteration):
     plt.close()
-    círculo_exterior = plt.Circle((0.5, 0.5), 0.5, color='gray', fill=False)
-    círculo_interior = plt.Circle((0.5, 0.5), 0.3, color='gray', fill=False)
-    fig, ax = plt.subplots(figsize=(6, 6))  # Tamaño ajustado
-    #Add title
-    ax.set_title(f"t_max en {iteracion+1}" if iteracion != 0 else "t_max en 0")
-    ax.add_patch(círculo_exterior)
-    ax.add_patch(círculo_interior)
+    outer_circle = plt.Circle((0.5, 0.5), 0.5, color='gray', fill=False)
+    inner_circle = plt.Circle((0.5, 0.5), 0.3, color='gray', fill=False)
+    fig, ax = plt.subplots(figsize=(6, 6))  # Adjusted size
+    # Add title
+    ax.set_title(f"t_max at {iteration+1}" if iteration != 0 else "t_max at 0")
+    ax.add_patch(outer_circle)
+    ax.add_patch(inner_circle)
     
-    # Generar colores únicos para cada neurona
-    colores = [plt.cm.jet(i/float(len(pesos))) for i in range(len(pesos))]
+    # Generate unique colors for each neuron
+    colors = [plt.cm.jet(i/float(len(weights))) for i in range(len(weights))]
     
-    for i, (x, y) in enumerate(pesos):
-        plt.scatter(x, y, color=colores[i])
+    for i, (x, y) in enumerate(weights):
+        plt.scatter(x, y, color=colors[i])
     
     plt.draw()
     plt.show()
 
-def gas_neural(num_señales, num_neuronas, t_max):
+def neural_gas(num_signals, num_neurons, t_max):
     
     lambda_i = 10
     lambda_f = 0.01
     epsilon_i = 0.5
     epsilon_f = 0.05
 
-    # Inicialización de pesos
-    pesos = []
+    # Initialization of weights
+    weights = []
     i = 0
-    while len(pesos) < num_neuronas:
+    while len(weights) < num_neurons:
         w = (random.random(), random.random())
         if (w[0]-(0.5))**2+(w[1]-(0.5))**2 > 0.5**2 or (w[0]-(0.5))**2+(w[1]-(0.5))**2 < 0.3**2:
             continue
         else:
-            pesos.append(w)
+            weights.append(w)
 
-    # Bucle principal
+    # Main loop
     i = 0
     while i < t_max:
-        # Generación de señal aleatoria
+        # Generate random signal
         e = (random.random(), random.random())
         if (e[0]-(0.5))**2+(e[1]-(0.5))**2 > 0.5**2 or (e[0]-(0.5))**2+(e[1]-(0.5))**2 < 0.3**2:
             continue
         else:
-            distancias = []
-            for j in range(len(pesos)):
-                distancia = math.sqrt((pesos[j][0] - e[0])**2 + (pesos[j][1] - e[1])**2)
-                distancias.append((distancia, j))
-            distancias.sort()
-            for k in range(len(distancias)):
-                # Actualización de pesos
-                epsilon_para_t = (epsilon_i*(epsilon_f/epsilon_i)**((i+1)/t_max))
-                lambda_para_t = (lambda_i*(lambda_f/lambda_i)**((i+1)/t_max))
-                w_delta = epsilon_para_t*(math.exp((-k)/lambda_para_t))
-                w_delta_x = w_delta*(e[0]-pesos[distancias[k][1]][0])
-                w_delta_y = w_delta*(e[1]-pesos[distancias[k][1]][1])
-                pesos[distancias[k][1]] = (pesos[distancias[k][1]][0]+w_delta_x, pesos[distancias[k][1]][1]+w_delta_y)
-            
+            distances = []
+            for j in range(len(weights)):
+                distance = math.sqrt((weights[j][0] - e[0])**2 + (weights[j][1] - e[1])**2)
+                distances.append((distance, j))
+            distances.sort()
+            for k in range(len(distances)):
+                # Update weights
+                epsilon_t = (epsilon_i*(epsilon_f/epsilon_i)**((i+1)/t_max))
+                lambda_t = (lambda_i*(lambda_f/lambda_i)**((i+1)/t_max))
+                
+                # Calculate the weight update delta based on the learning rate (epsilon_t), distance (lambda_t), and difference between the signal and weight position
+                w_delta = epsilon_t * math.exp((-k) / lambda_t)
+                w_delta_x = w_delta * (e[0] - weights[distances[k][1]][0])
+                w_delta_y = w_delta * (e[1] - weights[distances[k][1]][1])
+                
+                # Update the weight at index distances[k][1] by adding the weight update delta to its x and y coordinates
+                weights[distances[k][1]] = (weights[distances[k][1]][0]+w_delta_x, weights[distances[k][1]][1]+w_delta_y)
             if i == 0 or i == 299 or i == 2499 or i == 39999:
-                plot_weights(pesos, e, i)
+                plot_weights(weights, e, i)
         i += 1
 
-# Ejemplo de uso con 200 señales y 50 neuronas
-gas_neural(200, 400, 40000)
+# Example usage with 200 signals and 50 neurons
+neural_gas(200, 400, 40000)
